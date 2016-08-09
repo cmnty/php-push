e2d00ae7f62b362ccf35f8e7b21f1935baa54ab5
Johan de Ruijter <johan@cmnty.com>
2016-08-09 11:09:54 +0200

    Allow notifications to be send async

--- src/Client.php
+++ src/Client.php
@@ -4,6 +4,7 @@ namespace Cmnty\Push;
 
 use Cmnty\Push\Crypto\Cryptograph;
 use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Promise\PromiseInterface;
 use Psr\Http\Message\ResponseInterface;
 
 class Client implements PushClient
@@ -38,17 +39,30 @@ class Client implements PushClient
      * @param PushNotification $notification
      * @param PushSubscription $subscription
      *
-     * @param ResponseInterface
+     * @return ResponseInterface
      */
     public function pushNotification(PushNotification $notification, PushSubscription $subscription)
+    {
+        return $this->pushNotificationAsync($notification, $subscription)->wait();
+    }
+
+    /**
+     * Send a push notification asynchronously.
+     *
+     * @param PushNotification $notification
+     * @param PushSubscription $subscription
+     *
+     * @return PromiseInterface
+     */
+    public function pushNotificationAsync(PushNotification $notification, PushSubscription $subscription)
     {
         $cipher = $this->cryptograph->encrypt($notification, $subscription);
 
         $pushMessage = new Message($cipher, $subscription);
 
         $request = $this->pushService->createRequest($pushMessage, $subscription);
-        $response = $this->httpClient->send($request);
+        $promise = $this->httpClient->sendAsync($request);
 
-        return $response;
+        return $promise;
     }
 }
